11e283c6c9b3270ddd92a921a86088a348b71761
DedalJun <bohdanych2011@gmail.com>
2020-07-09 16:38:53 +0300

    Initial commit

---
+++ src/main/java/com/appspot/hword282722/TestServlet.java
@@ -0,0 +1,14 @@
+package com.appspot.hword282722;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TestServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/TestServlet.jsp").forward(request, response);
+    }
+}
---
+++ src/main/webapp/TestServlet.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Админ
+  Date: 09.07.2020
+  Time: 16:16
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Servlet</title>
+</head>
+<body>
+Hi! It`s servlet page!
+</body>
+</html>
